861e3937184ae5136dbb6fb8fde5a7b86b0bbfd7
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-03 14:52:24 +0600

    type of variable

---
+++ fundamental Concept/variable.js
@@ -0,0 +1,12 @@
+// ********
+// Variable === Container 
+// ********
+
+// Number type variable 
+var sunGlassPrice = 25;
+
+// String type variable 
+var subjectName = "Science";
+
+// Boolean type variable
+var passed = true;
