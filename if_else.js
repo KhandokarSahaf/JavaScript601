963c0512d6a308dded87ecaa710b3e06650f7d4f
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-18 14:28:25 +0600

    first commit

---
+++ if_else.js
@@ -0,0 +1,9 @@
+const salary = 25000;
+const isBSC = true;
+let age = 25;
+
+if((salary>20000 || age>20) && isBSC==true){
+    console.log("tumi to sera patro");
+}else{
+    console.log("sorry bhai hobe nh biye");
+}
