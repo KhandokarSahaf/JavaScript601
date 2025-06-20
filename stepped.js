e6c6cbd0b1dda6bdc9dc40079c0bc743ebaf5e1a
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-20 12:01:45 +0600

    stepped.js added

--- stepped.js
+++ stepped.js
@@ -1,8 +1,17 @@
-let amount = 5000;
-let discount = 10;
+let amount = 2500;
 
 if (amount >= 5000) {
   discount = (amount * 10) / 100;
   amount = amount - discount;
-  console.log(amount);
+  console.log(`after apply 10% discount payable amount is ${amount} taka`);
+} else if (amount >= 3000) {
+  discount = (amount * 5) / 100;
+  amount = amount - discount;
+  console.log(`after putting 5% discount you have to pay me ${amount} taka`);
+} else if (amount >= 2000) {
+  discount = (amount * 4) / 100;
+  amount = amount - discount;
+  console.log(`after adding 2% discount total amount is ${amount} taka`);
+} else{
+    console.log(`Your total amount is ${amount} Taka only`);
 }
