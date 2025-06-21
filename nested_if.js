beda4dab33165855a095f7e18c480723f92bf5a3
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-21 07:09:01 +0600

    nested_if.js added

---
+++ nested_if.js
@@ -0,0 +1,15 @@
+const money = 380;
+
+if(money >= 400){
+    console.log(`tui to mama borolok`);
+} else{
+    if(money >= 100){
+        console.log(`tui goribo nh borolok oo nh`);
+    } else{
+        if(money >= 10){
+            console.log(`kola kha calcium bara`);
+        } else{
+            console.log(`tui amar bondhu nh`);
+        }
+    }
+}
