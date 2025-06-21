2f070b986561670468944fbadcfd56847f239160
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-21 07:42:32 +0600

    ternary.js

---
+++ ternary.js
@@ -0,0 +1,12 @@
+// TERNARY
+const totalPrice = 300;
+
+// if (totalPrice > 200) {
+//   console.log(`discount 10 taka`);
+// } else {
+//   console.log(`no discount`);
+// }
+
+// This is short version of if else condition using ternary
+
+totalPrice > 200 ? console.log(`discount 10 taka`) : console.log(`no discount`);
