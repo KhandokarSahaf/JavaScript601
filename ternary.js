07a01d02624ee5751a178387d485e621205bec4b
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-21 13:19:20 +0600

    ternary.js

--- ternary.js
+++ ternary.js
@@ -1,4 +1,4 @@
-// TERNARY
+// TERNARY 01
 const totalPrice = 300;
 
 // if (totalPrice > 200) {
@@ -8,5 +8,19 @@ const totalPrice = 300;
 // }
 
 // This is short version of if else condition using ternary
-
 totalPrice > 200 ? console.log(`discount 10 taka`) : console.log(`no discount`);
+
+// advance ternary 02
+let price = 500;
+let leader = true;
+
+// if (price > 500) {
+//   price = 0;
+// } else {
+//   price = price + 100;
+// }
+// console.log(` total price is ${price}`);
+
+// This is short version of if else condition using ternary
+price = leader === true ? 0 : (price = price + 100);
+console.log(price);
