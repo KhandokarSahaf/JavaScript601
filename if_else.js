4be4895d26e64fbd64a7e1c11ec7f92b660397a2
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-21 20:32:06 +0600

    fair player and game type validation added in if_elsejs

--- if_else.js
+++ if_else.js
@@ -1,9 +1,23 @@
+// const { use } = require("react");
+
 const salary = 25000;
 const isBSC = true;
 let age = 25;
 
-if((salary>20000 || age>20) && isBSC==true){
-    console.log("tumi to sera patro");
-}else{
-    console.log("sorry bhai hobe nh biye");
+if ((salary > 20000 || age > 20) && isBSC == true) {
+  console.log("tumi to sera patro");
+} else {
+  console.log("sorry bhai hobe nh biye");
+}
+
+// *****
+
+let gameLevel = 70;
+let accountType = "gmail";
+let useHack = true;
+
+if ((gameLevel >= 60 && accountType === true) || !useHack === true) {
+  console.log(`you are panel user`);
+} else {
+  console.log(`you are a fair player`);
 }
