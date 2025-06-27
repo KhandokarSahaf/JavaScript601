54b6f46e088afb2827de813b9b730279ec49dca4
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-27 18:14:24 +0600

    push js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fdName = ['Farad', 'Sam', 'Shahab',' Don', 'Sharif'];
+const fdName = ["Farad", "Sam", "Shahab", " Don", "Sharif"];
 
 console.log(fdName[2]);
-// Shahab to Sahaf Name change 
+// Shahab to Sahaf Name change
 fdName[2] = "Sahaf";
 console.log(fdName);
---
+++ push-pop.js
@@ -0,0 +1,7 @@
+const friends = ['siam', 'samir', 'dihan', 'sadman'];
+
+friends.push('sahaf');
+console.log(friends);
+friends.pop();
+friends.pop();
+console.log(friends);
