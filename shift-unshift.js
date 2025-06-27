563c858e693f072b07219f98e901009c76fd9013
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-27 18:26:05 +0600

    shift and unshift leaning

--- shift-unshift.js
+++ shift-unshift.js
@@ -1,8 +1,8 @@
-const name = ['balam', 'kalam', 'salam'];
+const name = ["balam", "kalam", "salam"];
 console.log(name);
-// Shift used for remove element from start 
+// Shift used for remove element from start
 name.shift();
 console.log(name);
-// Unshift used for add new element for start 
-name.unshift('SAHAF');
+// Unshift used for add new element for start
+name.unshift("SAHAF");
 console.log(name);
