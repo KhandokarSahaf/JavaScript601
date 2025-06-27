dc07ec9d6ddbefb325614164060278703bb6596f
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-27 19:41:19 +0600

    slice added

--- concat.js
+++ concat.js
@@ -1,5 +1,4 @@
-const data1 = [1, 2,3,4,5];
-const data2 = [6,7,8,9,10];
-
+const data1 = [1, 2, 3, 4, 5];
+const data2 = [6, 7, 8, 9, 10];
 
 console.log(data1.concat(data2));
---
+++ slice.js
@@ -0,0 +1,11 @@
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 4);
+
+console.log(citrus);
+
+
+// total index + (-slice)= actual slice 
+const Fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const myBest = Fruits.slice(-3, -1);
+
+console.log(myBest);
