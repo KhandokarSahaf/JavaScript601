c19f7421df22c0b9bbfc3c3a22c47d883c483a81
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-27 18:39:32 +0600

    includes.js added in this section

---
+++ includes.js
@@ -0,0 +1,9 @@
+const friends = ['sahaf', 'sayam', 'dihan', 'sadman', 'sayma'];
+
+console.log(friends.includes('sahaf'));
+
+if(friends.includes('sadman')){
+    console.log('sadman is here lets gooo..')
+} else{
+    console.log('wait for sadman')
+}
