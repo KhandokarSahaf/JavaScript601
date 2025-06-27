6c14d48bad4bd85fb8c18a6762bbfe7e2622ab75
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-06-27 18:51:11 +0600

    isArray added in this commit

---
+++ isArray.js
@@ -0,0 +1,9 @@
+const friends = ["sahaf", "sayam", "dihan", "sadman", "sayma"];
+const food = " water mellon ";
+const num = [12, 32, 92, 43];
+const isOld = true;
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(food));
+console.log(Array.isArray(num));
+console.log(Array.isArray(isOld));
